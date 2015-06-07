805cf7860262482c43b1195b32a7c534d69998ee
Arne Kuederle <a.kuederle@gmail.com>
2015-06-07 17:48:59 +0200

    replottet everthing using way2

--- example1/example1.py
+++ example1/example1.py
@@ -17,12 +17,20 @@ x_values = np.linspace(100, 1000, 250)
 normalised_data = normalised_data[19:] # cutting the data
 x_values = x_values[19:]
 
+fig = plt.figure("My awesome Plot", figsize=(8, 4), dpi=100)
+ax = fig.add_subplot(111)
+ax.plot(x_values, normalised_data)
 
-plt.plot(x_values, normalised_data)
-plt.xlabel("wavenumber $\lambda$ [nm]")
-plt.ylabel("intesity [V]")
 
-plt.xlim(x_values.min(), x_values.max())
-plt.ylim(0.8*normalised_data.min(), 1.1*normalised_data.max())
+ax.set_xlabel("wavenumber $\lambda$ [nm]")
+ax.set_ylabel("intesity [V]")
 
-plt.grid(True)
+ax.set_xlim(x_values.min(), x_values.max())
+ax.set_ylim(0.8*normalised_data.min(), 1.1*normalised_data.max())
+
+ax.grid(True)
+
+fig.tight_layout()
+fig.savefig("./example1.pdf")
+
+fig.show()
